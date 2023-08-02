c1cd0c0c1e995310e20d46e312fa6eef574b95b8
Erick_Prado404 <73625446+ErickPrado101@users.noreply.github.com>
2023-08-02 14:40:22 -0300

    Create database.php

---
+++ database.php
@@ -0,0 +1,24 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('description');
+            $table->decimal('amount', 10, 2);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
